97f7ae3c012031333c9148a7388c9faf16bd3139
Jasmin <yasmeenmohaisen@gmail.com>
2019-05-03 22:47:27 +0300

    added pick pic

--- app/src/main/java/com/example/jasminapp/PicFragment.java
+++ app/src/main/java/com/example/jasminapp/PicFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,21 +14,50 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class PicFragment extends Fragment {
 
-    android.support.v7.widget.Toolbar toolbar;
-    Button btn;
+    private static final int IMAGE_PICK_CODE = 100;
+    private static final int PERMISSION_CODE = 1001;
+    ImageView imageView;
+    Button gallery;
+    Uri imageUri;
 
     @Override
     public View onCreateView( LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pic,container,false);
+        gallery = view.findViewById(R.id.gal_btn);
+        imageView = view.findViewById(R.id.pic);
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+
+
+        });
 
         return view;
 
 
     }
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery,IMAGE_PICK_CODE);
+    }
 
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if( requestCode == IMAGE_PICK_CODE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
 }
+
+
+
